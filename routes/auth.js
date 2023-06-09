1d0e72e4e041316ba3a778862ef3160e966e79b3
Moisés (Portátil Daiara) <moisesmorentes@gmail.com>
2023-06-09 14:28:41 +0200

    Ahora se pueden eliminar contactos. Pendiente por solucionar el bug que impide al servidor enviar una respuesta

--- routes/auth.js
+++ routes/auth.js
@@ -1,45 +1,90 @@
 const express = require("express")
-const jwt = require("jsonwebtoken");
 const bcrypt = require("bcrypt");
-const controller = require("../controller/controller")
 const db = require("../controller/db");
 const fs = require("fs")
+const util = require("util")
 
 const router = express.Router()
 
-router.post("/register", (req, res) => {
-	const { user, pass, email } = req.body
+const mkdir = util.promisify(fs.mkdir)
+const rmdir = util.promisify(fs.rm)
 
-	const hashPsw = bcrypt.hashSync(pass, 10)
+router.post("/register", async (req, res) => {
+	try {
+		const { user, pass, email } = req.body
 
-	db.query(
-		"SELECT user FROM users WHERE user = ? AND email = ?",
-		[ user, email ],
-		(error, resultados) => {
-			if(error || resultados.length > 0) {
-				res.status(401).send("Error. el usuario o el email ya existen")
-				console.log(error)
-			} else {
-				db.query(
-					"INSERT INTO users (user, pass, email, storage) VALUES (?, ?, ?, ?)",
-					[user, hashPsw, email, user],
-					error => {
-						if(error) {
-							res.status(500).send("Error al registrar el usuario")
-						} else {
-
-							fs.mkdir(`./storage/${user}`, error => {
-								if(error) console.log(error)
-								else console.log("Carpeta creada con éxito")
-							})
-							res.status(201).send("Usuario creado correctamente")
-						}
-					}
-				)
-			}
+		if(!user || !pass || !email) {
+			res.status(400).send("Error. Falta usuario, contraseña o email")
+			return
 		}
-	)
 
+		const hashPsw = bcrypt.hashSync(pass, 10)
+
+		const resultados = await db.query(
+			"SELECT user FROM users WHERE user = ? AND email = ?",
+			[user, email]
+		)
+
+		if(resultados > 0) {
+			res.status(401).send("Error. El usuario o email ya existe")
+			return
+		}
+
+		await db.query(
+			"INSERT INTO users (user, pass, email, storage) VALUES (?, ?, ?, ?)",
+			[user, hashPsw, email, user]
+		)
+
+			await mkdir(`./storage/${user}`)
+			console.log("Carpeta creada con éxito")
+			res.status(201).send("Usuario creado con éxito")
+	} catch (error) {
+		console.log(error);
+    res.status(500).send("Error al registrar el usuario")
+	}
 })
 
+
+// FIXME Error al eliminar un usuario. Problema de permisos (Solo sucede en thunder client)
+// FIXME La petición se recibe y el servidor la procesa. Pero no envía respuesta
+router.delete("/", async (req, res) => {
+  const { user } = req.body;
+
+  try {
+    if (!user) {
+      res.status(400).send("Error. No se ha introducido un usuario");
+      return;
+    }
+
+    const resultado = await db.query(
+      "SELECT user FROM users WHERE user = ?",
+      [user]
+    );
+
+    if (resultado.length === 0) {
+      res.status(400).send("Error. El usuario no existe");
+      return;
+    }
+
+    await db.query("DELETE FROM users WHERE user = ?", [user]);
+
+    await rmdir(`./storage/${user}`, { recursive: true }, (error) => {
+      if (error) {
+        res.status(500).send("La carpeta no existe");
+        return;
+      }
+      console.log("Carpeta eliminada con éxito");
+    });
+
+		// BUG No se alcanza esta parte del código
+    res.status(204).send("Usuario eliminado con éxito");
+
+  } catch (error) {
+    res.status(500).send("Error al eliminar el usuario");
+
+    console.log(error);
+  }
+});
+
+//TODO Implementar sistema de autentificación con JWT
 module.exports = router
